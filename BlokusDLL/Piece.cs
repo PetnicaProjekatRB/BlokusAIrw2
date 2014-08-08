ccdac65cb0df29acca788ffe26cbbc9a0cf74453
StarGate <nikola.bebic99@gmail.com>
2014-08-08 22:27:16 +0200

    Innitial commit

---
+++ BlokusDLL/Piece.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace BlokusDLL
+{
+    struct Piece
+    {
+        public int[][] coords;
+        public int[][] liberty;
+
+        public Piece Rot90()
+        {
+            var p = new Piece(coords.Length,liberty.Length);
+            for (int i = 0; i < coords.Length; i++)
+            {
+                p.coords[i] = new int[2];
+                p.coords[i][0] =  this.coords[i][1];
+                p.coords[i][1] = -this.coords[i][0];
+            }
+
+            for (int i = 0; i < liberty.Length; i++)
+            {
+                p.liberty[i] = new int[2];
+                p.liberty[i][0] =  this.liberty[i][1];
+                p.liberty[i][1] = -this.liberty[i][0];
+            }
+            return p;
+        }
+
+        public Piece ReflectY()
+        {
+            var p = new Piece(coords.Length, liberty.Length);
+            for (int i = 0; i < coords.Length; i++)
+            {
+                p.coords[i] = new int[2];
+                p.coords[i][0] = -this.coords[i][0];
+                p.coords[i][1] = this.coords[i][1];
+            }
+            for (int i = 0; i < liberty.Length; i++)
+            {
+                p.liberty[i] = new int[2];
+                p.liberty[i][0] = -this.liberty[i][0]; 
+                p.liberty[i][1] = this.liberty[i][1];
+            }
+            return p;
+        }
+
+        public Piece(int i, int j)
+        {
+            coords = new int[i][];
+            liberty = new int[j][];
+        }
+    }
+}
---
+++ BlokusDLL/PieceLoader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace BlokusDLL
+{
+    class PieceLoader
+    {
+        public struct Komadici
+        {
+            public Piece[] pieces;
+        }
+
+        public struct KomadiciOrbiatle
+        {
+            public Piece[][] orbitale;
+        }
+
+        public static void LoadPieces()
+        {
+            var deckText = File.ReadAllText("C:\\komadici.json");
+            Komadici komadi = JsonConvert.DeserializeObject<Komadici>(deckText);
+            var matricaSvega = new KomadiciOrbiatle();
+            matricaSvega.orbitale = new Piece[komadi.pieces.Length][];
+            for (int i = 0; i < komadi.pieces.Length; i++)
+            {
+                matricaSvega.orbitale[i] = new Piece[8];
+                matricaSvega.orbitale[i][0] = komadi.pieces[i];
+                matricaSvega.orbitale[i][1] = komadi.pieces[i].Rot90();
+                matricaSvega.orbitale[i][2] = komadi.pieces[i].Rot90().Rot90();
+                matricaSvega.orbitale[i][3] = komadi.pieces[i].Rot90().Rot90().Rot90();
+                matricaSvega.orbitale[i][4] = komadi.pieces[i].ReflectY();
+                matricaSvega.orbitale[i][5] = komadi.pieces[i].ReflectY().Rot90();
+                matricaSvega.orbitale[i][6] = komadi.pieces[i].ReflectY().Rot90().Rot90();
+                matricaSvega.orbitale[i][7] = komadi.pieces[i].ReflectY().Rot90().Rot90().Rot90();
+            }
+            var orbitaleText = JsonConvert.SerializeObject(matricaSvega);
+            File.WriteAllText("C:\\komadiciOrbitale.json", orbitaleText);
+        }
+
+        public static int Main(string[] argv)
+        {
+            LoadPieces();
+            return 0;
+        }
+    }
+}
