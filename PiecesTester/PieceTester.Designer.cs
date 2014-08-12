6e365611067e7b937b090485d457537e595aacaf
StarGate <nikola.bebic99@gmail.com>
2014-08-12 16:06:25 +0200

    PiecesTester.csproj

---
+++ PiecesTester/PieceTester.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace PiecesTester
+{
+    partial class PieceTester
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPiece = new System.Windows.Forms.Button();
+            this.txtPieceName = new System.Windows.Forms.TextBox();
+            this.btnOrbitPrev = new System.Windows.Forms.Button();
+            this.btnOrbitNext = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPiece
+            // 
+            this.btnPiece.Location = new System.Drawing.Point(614, 38);
+            this.btnPiece.Name = "btnPiece";
+            this.btnPiece.Size = new System.Drawing.Size(148, 25);
+            this.btnPiece.TabIndex = 0;
+            this.btnPiece.Text = "Get Piece";
+            this.btnPiece.UseVisualStyleBackColor = true;
+            this.btnPiece.Click += new System.EventHandler(this.btnPiece_Click);
+            // 
+            // txtPieceName
+            // 
+            this.txtPieceName.Location = new System.Drawing.Point(614, 12);
+            this.txtPieceName.Name = "txtPieceName";
+            this.txtPieceName.Size = new System.Drawing.Size(148, 20);
+            this.txtPieceName.TabIndex = 1;
+            // 
+            // btnOrbitPrev
+            // 
+            this.btnOrbitPrev.Location = new System.Drawing.Point(614, 69);
+            this.btnOrbitPrev.Name = "btnOrbitPrev";
+            this.btnOrbitPrev.Size = new System.Drawing.Size(72, 23);
+            this.btnOrbitPrev.TabIndex = 2;
+            this.btnOrbitPrev.Text = "Prev Orbit";
+            this.btnOrbitPrev.UseVisualStyleBackColor = true;
+            this.btnOrbitPrev.Click += new System.EventHandler(this.btnOrbitPrev_Click);
+            // 
+            // btnOrbitNext
+            // 
+            this.btnOrbitNext.Location = new System.Drawing.Point(692, 69);
+            this.btnOrbitNext.Name = "btnOrbitNext";
+            this.btnOrbitNext.Size = new System.Drawing.Size(70, 23);
+            this.btnOrbitNext.TabIndex = 3;
+            this.btnOrbitNext.Text = "Next Orbit";
+            this.btnOrbitNext.UseVisualStyleBackColor = true;
+            this.btnOrbitNext.Click += new System.EventHandler(this.btnOrbitNext_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(614, 464);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PieceTester
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 499);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnOrbitNext);
+            this.Controls.Add(this.btnOrbitPrev);
+            this.Controls.Add(this.txtPieceName);
+            this.Controls.Add(this.btnPiece);
+            this.Name = "PieceTester";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PieceTester_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPiece;
+        private System.Windows.Forms.TextBox txtPieceName;
+        private System.Windows.Forms.Button btnOrbitPrev;
+        private System.Windows.Forms.Button btnOrbitNext;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PiecesTester/PieceTester.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using BlokusDll;
+
+namespace PiecesTester
+{
+    public partial class PieceTester : Form
+    {
+        KomadiciOrbitale koms;
+        GameGrid grid = new GameGrid();
+        Piece[] pieceFamily;
+        int orbitCounter;
+
+        public PieceTester()
+        {
+            InitializeComponent();
+            koms = PieceLoader.LoadPieces();
+        }
+
+        private void btnPiece_Click(object sender, EventArgs e)
+        {
+            foreach(var a in koms.orbitale)
+            {
+                if (a[0].name == txtPieceName.Text)
+                {
+                    pieceFamily = a;
+                    orbitCounter = 0;
+                    updateBoard();
+                    return;
+                }
+            }
+        }
+
+        private void updateBoard()
+        { 
+            grid = new GameGrid();
+            grid.Place(5, 5, pieceFamily[orbitCounter], Player.PL1, false);
+            PieceTester_Paint(null, null);
+        }
+
+        private void btnOrbitPrev_Click(object sender, EventArgs e)
+        {
+            if (orbitCounter > 0)
+            {
+                orbitCounter--;
+                updateBoard();
+            }
+        }
+
+        private void btnOrbitNext_Click(object sender, EventArgs e)
+        {
+            if (orbitCounter < pieceFamily.Length - 1)
+            {
+                orbitCounter++;
+                updateBoard();
+            }
+        }
+
+        private void PieceTester_Paint(object sender, PaintEventArgs e)
+        {
+            using(var graphics = CreateGraphics())
+            {
+                graphics.Clear(this.BackColor);
+                using (var pen = new Pen(Color.FromArgb(255, 64, 64, 64)))
+                { 
+                    for (int i = 0; i < 15; i++)
+                    { 
+                        graphics.DrawLine(pen, 0, i * 21, 294, i * 21);
+                        graphics.DrawLine(pen, i * 21, 0, i * 21, 294);
+                    }
+                }
+
+                using (var brushOrange = new SolidBrush(Color.Orange))
+                {
+                    using (var brushPurple = new SolidBrush(Color.Purple))
+                    {
+                        foreach (var square in grid.Squares)
+                        {
+                            var ax = square.Key.Item1 * 21 + 1;
+                            var ay = 295 - (square.Key.Item2 * 21 + 21);
+                            var pl = square.Value.Owner;
+
+                            graphics.FillRectangle((pl == Player.PL1) ? brushOrange : brushPurple,
+                                ax, ay, 20, 20);
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PieceLoader.RebuildPieces();
+            koms = PieceLoader.LoadPieces();
+        }
+
+
+    }
+}
